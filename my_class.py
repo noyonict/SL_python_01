e0799029e8aeb3f71f35957f4072bda4316c09f4
noyon <mohaymenul.islam@mulytic.io>
2019-11-29 00:09:11 +0600

    add a student info system

--- my_class.py
+++ my_class.py
@@ -1,39 +1,94 @@
 
-class Student:
-    def __init__(self, name, institute, email, phone):
-        self.name = name
-        self.institute = institute
-        self.email = email
-        self.phone = phone
 
-    def __str__(self):
-        return self.name
-
-    def show_info(self):
-        print('*' * 50)
-        print('Name:', self.name)
-        print('Institute:', self.institute)
-        print('Email:', self.email)
-        print('Phone:', self.phone)
-        print()
-
-    def change_phone(self, new_number):
-        self.phone = new_number
-
-    def change_email(self, new_email):
-        self.email = new_email
+def add_student():
+    s_id = input('Enter student ID: ')
+    name = input('Enter student name: ')
+    email = input('Enter student email: ')
+    phone = input('Enter student phone: ')
+    with open('student_db.txt', 'a') as f:
+        f.write(s_id + ',' + name + ',' + email + ',' + phone + '\n')
 
 
-salman = Student('Salman', 'DPI', 'salman@gmail.com', '01928493916')
-abdullah = Student('Abdullah', 'DPI', 'salman@gmail.com', '01928493916')
+def edit_student():
+    given_s_id = input('Enter the student id which you want to edit: ')
+    with open('student_db.txt', 'r') as f:
+        info = f.read()
+        with open('student_db.txt', 'w') as fw:
+            for i in info.split('\n'):
+                # print(i)
+                if i:
+                    s_id, name, email, phone = i.split(',')
+                    if int(given_s_id) == int(s_id):
+                        print('1 - Name')
+                        print('2 - Email')
+                        print('3 - Phone')
+                        choice = int(input('what you want to edit?'))
+                        if choice == 1:
+                            name = input('Enter a new student name: ')
+                        elif choice == 2:
+                            email = input('Enter a new student email: ')
+                        elif choice ==  3:
+                            phone = input('Enter a new student phone: ')
+                        print(given_s_id, 'is edited!')
+                    fw.write(s_id + ',' + name + ',' + email + ',' + phone + '\n')
 
-salman.show_info()
-abdullah.show_info()
-abdullah.change_phone('01775748710')
-abdullah.show_info()
-salman.change_email('salman145122@bsdi-bd.org')
-salman.show_info()
+
+def view_student():
+    with open('student_db.txt', 'r') as f:
+        info = f.read()
+        for i in info.split('\n'):
+            # print(i)
+            if i:
+                s_id, name, email, phone = i.split(',')
+                print('*' * 40)
+                print('Student ID:', s_id)
+                print('Student Name:', name)
+                print('Student E-mail:', email)
+                print('Student Phone:', phone)
+                print('*' * 40)
 
 
+def delete_student():
+    given_s_id = input('Enter the student id which you want to delete: ')
+    with open('student_db.txt', 'r') as f:
+        info = f.read()
+        with open('student_db.txt', 'w') as fw:
+            for i in info.split('\n'):
+                # print(i)
+                if i:
+                    s_id, name, email, phone = i.split(',')
+                    if int(given_s_id) == int(s_id):
+                        print(given_s_id, 'is deleted!')
+                        continue
+                    else:
+                        fw.write(s_id + ',' + name + ',' + email + ',' + phone + '\n')
 
+
+def user_choice():
+    print('1 - Add New Student')
+    print('2 - View All Student')
+    print('3 - Edit a Student')
+    print('4 - Delete a Student')
+    return int(input('Enter your choice: '))
+
+
+def main():
+    while True:
+        u_choice = user_choice()
+        if u_choice == 1:
+            add_student()
+        elif u_choice == 2:
+            view_student()
+        elif u_choice == 3:
+            edit_student()
+        elif u_choice == 4:
+            delete_student()
+        again = input('Do you want to do it again? (y/n): ')
+        if again == 'n' or again == 'N':
+            print('Thank you! See you soon!')
+            break
+
+
+if __name__ == '__main__':
+    main()
 
